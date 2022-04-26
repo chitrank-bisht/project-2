b0e124a0d84d252af70a4308ff31b954508228ad
chitrank-bisht <chitrank229@gmail.com>
2022-04-26 14:55:37 +0530

    update drawnumber.py

--- drawnumber.py
+++ drawnumber.py
@@ -137,7 +137,7 @@ class grid(object):
 
 
 def guess(li):
-    model = tf.keras.models.load_model('m.model')
+    model = tf.keras.models.load_model('epic_num_reader.model')
 
     predictions = model.predict(li)
     print(predictions[0])
